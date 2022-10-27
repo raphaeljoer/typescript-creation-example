3d262b959d8011ce7fdaa41f99d079bee64e81a4
raphaeljoer <ra.joer@gmail.com>
2022-10-27 19:20:36 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-creation-example
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-bjdrfs)
---
+++ index.ts
@@ -0,0 +1,33 @@
+type InvoiceInput = {
+  id: string;
+  total_amount: number;
+};
+
+interface IInvoice {
+  id: string;
+  totalAmount: number;
+}
+
+export class Invoice implements IInvoice {
+  id: string;
+  totalAmount: number;
+
+  private constructor(input: InvoiceInput) {
+    this.id = input.id;
+    this.totalAmount = input.total_amount;
+  }
+
+  static create(input: InvoiceInput): Readonly<IInvoice> {
+    const invoice = new Invoice(input);
+    return JSON.parse(JSON.stringify(invoice));
+  }
+}
+
+const input: InvoiceInput = {
+  id: '123',
+  total_amount: 1250,
+};
+
+const invoice = Invoice.create(input);
+
+console.log(invoice);
