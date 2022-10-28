1f063c22ccad783e2ccbcab5bd68b2aef3085caa
raphaeljoer <ra.joer@gmail.com>
2022-10-28 09:38:41 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -5,7 +5,6 @@ import { tenantSettingsDataInput } from './data/tenantSettingsDataInput';
 
 const tentantAccuracySettings = new TentantAccuracySettings();
 const settings = tentantAccuracySettings.execute(tenantSettingsDataInput);
-
 const decimal = new DecimalMathAccuracy();
 const invoicePresentation = new InvoicePresentation(decimal, settings);
 const output = invoicePresentation.execute({ id: '123', total_amount: 1250 });
