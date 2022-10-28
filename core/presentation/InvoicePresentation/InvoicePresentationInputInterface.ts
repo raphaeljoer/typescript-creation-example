43c7b856744a2070690a14bed9e58294e9657eed
raphaeljoer <ra.joer@gmail.com>
2022-10-28 10:54:12 -0300

    1 file modified

--- core/presentation/InvoicePresentation/InvoicePresentationInputInterface.ts
+++ core/presentation/InvoicePresentation/InvoicePresentationInputInterface.ts
@@ -1,12 +1,12 @@
 export type InvoicePresentationInputInterface = {
   id: string;
-  external_id: string | null;
+  external_id: string;
   vendor: Partial<VendorInterface>;
   vendor_id: string | null;
   vendor_trade_name: string | null;
   due_date: string | null;
   created_at: string;
-  posting_date: string | null;
+  posting_date: string;
   discount_percent: string;
   invoice_total: string;
   invoice_total_in_system_currency: string;
@@ -16,21 +16,21 @@ export type InvoicePresentationInputInterface = {
   total_vat_tax_in_foreign_currency: string;
   vat_tax_percent: string;
   base_amount: string;
-  total_discount_amount: number | null;
-  total_discount_amount_in_system_currency: number | null;
-  total_discount_amount_in_foreign_currency: number | null;
-  total_down_payment_amount: number | null;
-  total_down_payment_amount_in_system_currency: number | null;
-  total_down_payment_amount_in_foreign_currency: number | null;
-  paid_to_date_amount: number | null;
-  paid_to_date_amount_in_system_currency: number | null;
-  paid_to_date_amount_in_foreign_currency: number | null;
-  additional_expenses: number | null;
-  additional_expenses_in_system_currency: number | null;
-  additional_expenses_in_foreign_currency: number | null;
-  notes: string | null;
-  external_ap_invoice_number: string | null;
-  reference_number_external: string | null;
+  total_discount_amount: string;
+  total_discount_amount_in_system_currency: string;
+  total_discount_amount_in_foreign_currency: string;
+  total_down_payment_amount: string;
+  total_down_payment_amount_in_system_currency: string;
+  total_down_payment_amount_in_foreign_currency: string;
+  paid_to_date_amount: string;
+  paid_to_date_amount_in_system_currency: string;
+  paid_to_date_amount_in_foreign_currency: string;
+  additional_expenses: string;
+  additional_expenses_in_system_currency: string;
+  additional_expenses_in_foreign_currency: string;
+  notes: string;
+  external_ap_invoice_number: string;
+  reference_number_external: string;
 };
 
 type VendorInterface = {
